6ad611c2b2a4a8a2de706d6ee3ddc0b3823b416a
szserrano <szserrano@mail.csuchico.edu>
2025-09-27 13:37:23 -0700

    Created Calculator. Move to Performance

--- Tpumps-Lead/app/(tabs)/_layout.tsx
+++ Tpumps-Lead/app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="calculator"
+        options={{
+          title: 'Calculator',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="dollarsign" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
